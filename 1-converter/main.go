9daac9d5c96f14fc659e191191831bd5c736192f
Айдар Шарафуллин <arsharafullin@sberbank.ru>
2024-10-29 21:26:47 +0300

    funcs

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -10,3 +10,21 @@ func main() {
 	const EUR = 1.5;
 	fmt.Print(rubCount * USD)
 }
+
+func coverter(count int, fiatFrom string, fiatTo string){
+	
+}
+
+func getUserInput() (float64, float64, float64) {
+	var count float64;
+	var fiatFrom float64;
+	var fiatTo float64;
+	fmt.Println("Введите данные");
+	fmt.Print("Введите количество денег: ");
+	fmt.Scan(&count);
+	fmt.Print("Валюта откуда: ");
+	fmt.Scan(&fiatFrom)
+	fmt.Print("Валюта куда: ");
+	fmt.Scan(&fiatTo);
+	return count, fiatFrom, fiatTo
+}
