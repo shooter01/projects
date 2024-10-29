695772b42b456267133c1d8162f516b2a8d76034
Айдар Шарафуллин <arsharafullin@sberbank.ru>
2024-10-29 21:21:12 +0300

    funcs

--- main.go
+++ main.go
@@ -6,8 +6,27 @@ import (
 )
 
 func main() {
-	var userHeight = 1.8;
-	var userKg float64 = 100;
-	var IMT = userKg / math.Pow(userHeight, 2)
-	fmt.Print(IMT)
+	userHeight, userKg := getUserInput();
+	var IMT = calculateIMT(userHeight, userKg)
+	outputResult(IMT)
+}
+
+func outputResult(IMT float64){
+	result := fmt.Sprintf("Ваш индекс %.2f", IMT)
+	fmt.Print(result)
+}
+
+func calculateIMT(userHeight, userKg float64) float64 {
+	return (userKg / math.Pow(userHeight / 100, 2))
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight float64;
+	var userKg float64;
+	fmt.Println("Калькулятор массы тела");
+	fmt.Print("Введите рост (в сантиметрах): ");
+	fmt.Scan(&userHeight);
+	fmt.Print("Введите вес: ");
+	fmt.Scan(&userKg);
+	return userHeight, userKg
 }
