cf0675fb6f9b166513de0298537bf660844dfd11
Айдар Шарафуллин <arsharafullin@sberbank.ru>
2024-11-08 23:06:30 +0300

    14.5 дешифровка

--- go-demo-4/encrypter/encrypter.go
+++ go-demo-4/encrypter/encrypter.go
@@ -40,5 +40,19 @@ func (enc *Encrypter) Encrypt(plainString []byte) []byte {
 }
 
 func (enc *Encrypter) Decrypt(encryptedString []byte) []byte {
-	return encryptedString
+	block, err := aes.NewCipher([]byte(enc.Key))
+	if err != nil {
+		panic(err.Error())
+	}
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	noncesize := aesGCM.NonceSize()
+	nonce, cipherText := encryptedString[noncesize:], encryptedString[:noncesize]
+	plainText, err := aesGCM.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plainText
 }
