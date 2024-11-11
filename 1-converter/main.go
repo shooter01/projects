66a97aec4fecf2dc015d29e9a086a77b34ea1934
Айдар Шарафуллин <arsharafullin@sberbank.ru>
2024-11-11 20:36:43 +0300

    ряд улучшений

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -5,26 +5,26 @@ import (
 )
 
 func main() {
-	var rubCount = 100.0;
-	const USD = 1.4;
-	const EUR = 1.5;
-	fmt.Print(rubCount * USD)
+	const USDToEURO = 0.94
+	const USDToRUB = 1.5
+	const EUROToRUB = USDToRUB / USDToEURO
+	fmt.Print(EUROToRUB)
 }
 
-func coverter(count int, fiatFrom string, fiatTo string){
-	
+func coverter(count int, fiatFrom string, fiatTo string) {
+
 }
 
 func getUserInput() (float64, float64, float64) {
-	var count float64;
-	var fiatFrom float64;
-	var fiatTo float64;
-	fmt.Println("Введите данные");
-	fmt.Print("Введите количество денег: ");
-	fmt.Scan(&count);
-	fmt.Print("Валюта откуда: ");
+	var count float64
+	var fiatFrom float64
+	var fiatTo float64
+	fmt.Println("Введите данные")
+	fmt.Print("Введите количество денег: ")
+	fmt.Scan(&count)
+	fmt.Print("Валюта откуда: ")
 	fmt.Scan(&fiatFrom)
-	fmt.Print("Валюта куда: ");
-	fmt.Scan(&fiatTo);
+	fmt.Print("Валюта куда: ")
+	fmt.Scan(&fiatTo)
 	return count, fiatFrom, fiatTo
 }
