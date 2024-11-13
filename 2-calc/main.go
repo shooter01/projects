31ee10e9e3cea3dcb60e33d3f5212486003db121
Айдар Шарафуллин <arsharafullin@sberbank.ru>
2024-11-13 10:08:00 +0300

    улучшения кода

--- 2-calc/main.go
+++ 2-calc/main.go
@@ -8,74 +8,75 @@ import (
 )
 
 func main() {
-	operation, userNumbers := getUserInput();
-	numbers := formatUserNumbers(userNumbers);
-	value := getResult(operation, numbers);
+	operation, userNumbers := getUserInput()
+	numbers := formatUserNumbers(userNumbers)
+	value := getResult(operation, numbers)
 
-
-	fmt.Println(operation )
-	fmt.Println(value )
+	fmt.Println(operation)
+	fmt.Println(value)
 }
 
 func getUserInput() (string, string) {
-	var operation string;
-	var userNumbers string;
-	fmt.Println("Введите операцию");
-	fmt.Print("Введите операцию (AVG, SUM, MED): ");
-	fmt.Scan(&operation);
-	
-	if (operation != "AVG" || operation != "MED" || operation != "SUM") {
+	var operation string
+	var userNumbers string
+	fmt.Println("Введите операцию")
+	fmt.Print("Введите операцию (AVG, SUM, MED): ")
+	fmt.Scan(&operation)
+	fmt.Println(operation)
+
+	operation = strings.Trim(operation, " ")
+
+	if operation != "AVG" && operation != "MED" && operation != "SUM" {
 		panic("Неправильный оператор")
 	}
 
-	fmt.Println("Введите числа через запятую: ");
+	fmt.Println("Введите числа через запятую: ")
 	fmt.Scan(&userNumbers)
 	return operation, userNumbers
 }
 
-func formatUserNumbers(stringToFromat string) ([]float64)  {
+func formatUserNumbers(stringToFromat string) []float64 {
 	numbers := strings.Split(stringToFromat, ",")
 	floatNumbers := []float64{}
 
-	for _, value:= range numbers {
+	for _, value := range numbers {
 		number, _ := strconv.ParseFloat(value, 64)
 		floatNumbers = append(floatNumbers, number)
 	}
 	return floatNumbers
 }
 
-func getResult(operationName string, numbers []float64) (float64)  {
+func getResult(operationName string, numbers []float64) float64 {
 	var total float64 = 0
-	for _, value:= range numbers {
+	for _, value := range numbers {
 		total += value
 	}
 	switch {
 	case operationName == "AVG":
-		return total/float64(len(numbers))
+		return total / float64(len(numbers))
 	case operationName == "SUM":
 		return total
 	case operationName == "MED":
 		return calculateMedian(numbers)
 	}
-	// numbers := strings.Split(stringToFromat, ",")
 	return total
 }
 
 func calculateMedian(data []float64) float64 {
-    dataCopy := make([]float64, len(data))
-    copy(dataCopy, data)
+	dataCopy := make([]float64, len(data))
+	copy(dataCopy, data)
 
-    sort.Float64s(dataCopy)
+	sort.Float64s(dataCopy)
 
-    var median float64
-    l := len(dataCopy)
-    if l == 0 {
-        return 0
-    } else if l%2 == 0 {
-        median = (dataCopy[l/2-1] + dataCopy[l/2]) / 2
-    } else {
-        median = dataCopy[l/2]
-    }
+	var median float64
+	l := len(dataCopy)
+	if l == 0 {
+		return 0
+	} else if l%2 == 0 {
+		median = (dataCopy[l/2-1] + dataCopy[l/2]) / 2
+	} else {
+		median = dataCopy[l/2]
+	}
 
-    return median
+	return median
 }
