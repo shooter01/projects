c9ba6f87c3af2fb1bbf498f3d393fac71649e58e
Mona Lisa <YOUR_EMAIL>
2025-02-05 18:30:43 +0300

    5.6 свой serveMux

--- go-adv-demo2/go.mod
+++ go-adv-demo2/go.mod
@@ -1,3 +1,3 @@
-module go-adv-demo2
+module go/adv-demo2
 
 go 1.23
--- go-adv-demo2/main.go
+++ go-adv-demo2/main.go
@@ -1,1 +1,22 @@
-package go_adv_demo2
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Hello World")
+}
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/hello", hello)
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("SERVER IS LISTENING")
+	server.ListenAndServe()
+}
