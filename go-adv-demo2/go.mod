831657c7a07a9bbb6bf1795d63f9f9b9fdefadd1
Mona Lisa <YOUR_EMAIL>
2025-02-10 19:43:08 +0300

    6.2 слайсы

--- go-adv-demo2/go.mod
+++ go-adv-demo2/go.mod
@@ -2,4 +2,4 @@ module go/adv-demo2
 
 go 1.23
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
--- go-demo-2/main.go
+++ go-demo-2/main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	transactions := [5]string{"1", "2"}
-	banks := [3]string{"test", "test2"}
-	fmt.Println(len(transactions[1:5]))
-	fmt.Println(banks)
+	transactionsNew := transactions
+	transactionsNew[0] = "30"
+	//banks := [3]string{"test", "test2"}
+	//fmt.Println(len(transactions[1:5]))
+	//fmt.Println(banks)
+	fmt.Println(transactions)
+	fmt.Println(transactionsNew)
 }
