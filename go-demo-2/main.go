9f414000611a318867fbdf6218274ebd24eee5ac
Mona Lisa <YOUR_EMAIL>
2025-02-12 19:43:13 +0300

    6.6 массив транзакий

--- go-demo-2/main.go
+++ go-demo-2/main.go
@@ -3,13 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	transactions := []string{"1", "2"}
+	transactions := []float64{}
+	maxIter := 3
+	for {
+		number := getTrabsaction()
+		transactions = append(transactions, number)
+		maxIter--
+		if maxIter < 0 {
+			break
+		}
+	}
 	//transactions[3] = "2"
 	//transactionsNew := transactions
-	transactions = append(transactions, "100")
+	//transactions = append(transactions, "100")
 	//banks := [3]string{"test", "test2"}
 	//fmt.Println(len(transactions[1:5]))
 	//fmt.Println(banks)
 	fmt.Println(transactions)
 	//fmt.Println(transactionsNew)
+}
+
+func getTrabsaction() float64 {
+	var number float64
+	fmt.Print("Enter number: ")
+	fmt.Scan(&number)
+	return number
 }
